9c3c671562be116aa0ff9ebcf51c67bbfa1e0867
Alexander <dankjjk@gmail.com>
2022-08-28 19:15:45 +0900

    Commit test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import './Header.scss';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="container headerContent">
                 <div className="headerLogo">
-                    <Link to="/"><img src="../img/logo.svg" alt="Logotype" /></Link>
+                    <Link to="/">
+                        <img src="../img/logo.svg" alt="Logotype" />
+                    </Link>
                 </div>
                 <div className="headerNav">
                     <ul>
-                        <li><Link to="/faq">Вопросы и ответы</Link></li>
-                        <li><Link to="/rules">Правила</Link></li>
-                        <li><Link to="/contacts">Контакты</Link></li>
+                        <li>
+                            <Link to="/faq">Вопросы и ответ</Link>
+                        </li>
+                        <li>
+                            <Link to="/rules">Правила</Link>
+                        </li>
+                        <li>
+                            <Link to="/contacts">Контакты</Link>
+                        </li>
                     </ul>
                     <div className="headerContact">
                         <a href="https://google.com" target="_blank" rel="noreferrer">
